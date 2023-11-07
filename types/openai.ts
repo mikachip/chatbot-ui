8fdb4f0f22d2bd592e374ef26c5101016847e8ee
mikachip <mikachip@artscale.ai>
2023-11-07 14:28:51 +1100

    tryfix

--- types/openai.ts
+++ types/openai.ts
@@ -14,7 +14,6 @@ export enum OpenAIModelID {
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_4 = 'gpt-4',
   GPT_4_0314 = 'gpt-4-0314',
-  GPT_4_32K = 'gpt-4-32k',
   GPT_4_1106 = 'gpt-4-1106-preview',
 }
 
@@ -57,18 +56,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     tokenLimit: 8000,
     requestLimit: 6000,
   },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'gpt-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-    requestLimit: 24000,
-  },
   [OpenAIModelID.GPT_4_1106]: {
     id: OpenAIModelID.GPT_4_1106,
     name: 'gpt-4-1106-preview',
-    maxLength: 512000,
-    tokenLimit: 128000,
-    requestLimit: 12000,
+    maxLength: 256000,
+    tokenLimit: 80000,
+    requestLimit: 8000,
   },
 };
